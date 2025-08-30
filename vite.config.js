1f39dafbf6bc81b9d3e0ddc42056289fa67a3ba1
YoChamp <yashvora2711@gmail.com>
2025-08-31 02:00:49 +0530

    Update base path and asset references for deployment
    - Changed the base path in vite.config.js from '/YV/' to '/Resume/'.
    - Updated asset paths in index.html to reflect the new '/Resume/' directory structure.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/YV/',
+  base: '/Resume/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
